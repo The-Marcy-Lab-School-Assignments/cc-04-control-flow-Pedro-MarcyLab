a2fd567da32e9b862851fe9c20ef422ad3d206bd
Pedro Delgado <pedro.delgado2017@mesacharter.org>
2021-10-05 15:20:46 +0000

    Completed problem 1

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 //1
-function fizzBuzz(){
-  
+function fizzBuzz() {
+    for(let i = 1; i <= 100; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log('FizzBuzz')
+        }else if (i % 5 === 0){
+            console.log('Buzz')
+        }else if(i % 3 === 0){
+            console.log('Fizz')
+        } else {
+            console.log(i)
+        }
+    }
 }
+fizzBuzz()
+/* log all numbers from 1-100
+if the number is a multiple of 3, log "Fizz"
+if the number is a multiple of 5, log "Buzz"
+if the number is a multiple of 3 & 5 log "FizzBuzz"
+*/
 
 //2
-function fizzBuzz(number){
+function fizzBuzz2(number){
   
 }
 
